db9a907a8009ccd4c1d0950b4673cad98bac0777
Aldhi Xar <aldhi.x@gmail.com>
2021-02-26 21:48:03 +0700

    small box

--- views/pages/admin/dashboard/index.blade.php
+++ views/pages/admin/dashboard/index.blade.php
@@ -3,12 +3,12 @@
 <x-admin::content-header>
     <div class="row mb-2">
       <div class="col-sm-6">
-        <h1>Blank Page</h1>
+        <h1>Dashboard</h1>
       </div>
       <div class="col-sm-6">
         <ol class="breadcrumb float-sm-right">
           <li class="breadcrumb-item"><a href="#">Home</a></li>
-          <li class="breadcrumb-item active">Blank Page</li>
+          <li class="breadcrumb-item active">Dashboard</li>
         </ol>
       </div>
     </div>
