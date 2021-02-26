ea4d67676a9ecb899052fba1a4fdac39407457f5
Aldhi Xar <aldhi.x@gmail.com>
2021-02-26 21:11:04 +0700

    table paginate

--- views/pages/admin/table/index.blade.php
+++ views/pages/admin/table/index.blade.php
@@ -29,9 +29,21 @@
 					<tr>
 						<td>Maman</td><td>maman@gmail.com</td><td></td>
 					</tr>
+					<tr>
+						<td>Andovi</td><td>andovi@gmail.com</td><td></td>
+					</tr>
 				</tbody>
 			</table>
 		</x-admin::card-body>
+		<x-admin::card-footer class="clearfix">
+			<ul class="pagination pagination-sm m-0 float-right">
+              <li class="page-item"><a class="page-link" href="#">&laquo;</a></li>
+              <li class="page-item"><a class="page-link" href="#">1</a></li>
+              <li class="page-item"><a class="page-link" href="#">2</a></li>
+              <li class="page-item"><a class="page-link" href="#">3</a></li>
+              <li class="page-item"><a class="page-link" href="#">&raquo;</a></li>
+            </ul>
+		</x-admin::card-footer>
 	</x-admin::card>
 </x-admin::content>
 @endsection
