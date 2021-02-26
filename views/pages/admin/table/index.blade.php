3c6b44434bbad3e5f42925d97e83e39b1ad3a7fb
Aldhi Xar <aldhi.x@gmail.com>
2021-02-26 21:05:16 +0700

    table search

--- views/pages/admin/table/index.blade.php
+++ views/pages/admin/table/index.blade.php
@@ -4,7 +4,20 @@
 	<h1> <i class="fas fa-table"></i> Table Example </h1>
 </x-admin::content-header>
 <x-admin::content>
-	<x-admin::card class="card-primay">
+	<x-admin::card>
+		<x-admin::card-header>
+			<h3 class="card-title">Table example</h3>
+			<div class="card-tools">
+				<form class="form-inline">
+					<x-admin::input-group class="input-group-sm">
+						<x-admin::input name="search"/>
+						<x-admin::append>
+							<button class="btn btn-secondary"><i class="fas fa-search"></i></button>
+						</x-admin::append>
+					</x-admin::input-group>
+				</form>
+			</div>
+		</x-admin::card-header>
 		<x-admin::card-body class="p-0">
 			<table class="table table-striped">
 				<thead>
